0a76bc11718859253b36657dec8413e67d03aa2c
Jake Atakhanov <jakeatakhanov@MacBook-Pro.local>
2023-07-13 01:59:45 -0400

    dashboard data fetched and updated api integrated

--- ticketsystem_mern/src/Pages/Dashboard/dashboard.page.js
+++ ticketsystem_mern/src/Pages/Dashboard/dashboard.page.js
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import { Container, Row, Col, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import TicketTable from "../../Components/Ticket-table/TicketTable.component";
 import PageBreadcrumb from "../../Components/Breadcrumb/PageBreadcrumb.component";
 import mockTickets from "../../Assets/data/mock-data.json";
+import { fetchAllTickets } from "../Ticket-List/ticketsAction";
 
 const Dashboard = () => {
+  const dispatch = useDispatch();
+  const { tickets } = useSelector((state) => state.tickets);
+
+  useEffect(() => {
+    if (!tickets.length) {
+      dispatch(fetchAllTickets());
+    }
+  }, [tickets, dispatch]);
+
+  const pendingTickets = tickets.filter((row) => row.status !== "Closed");
+  const totalTickets = tickets.length;
+
   return (
     <Container>
       <Row>
@@ -16,17 +30,19 @@ const Dashboard = () => {
       <Row>
         <Col className="text-center mt-5 mb-2">
           <Link to="/add-ticket">
-          <Button
-            variant="info"
-            style={{ fontSize: "2rem", padding: "10px 30px" }}>
-            Add New Ticket
-          </Button>
+            <Button
+              variant="info"
+              style={{ fontSize: "2rem", padding: "10px 30px" }}
+            >
+              Add New Ticket
+            </Button>
           </Link>
         </Col>
       </Row>
       <Row>
         <Col className="text-center mb-2">
-          <div> Total Tickets: 50 </div> <div> Pending Tickets: 5 </div>
+          <div> Total Tickets: {totalTickets} </div>{" "}
+          <div> Pending Tickets: {pendingTickets.length} </div>
         </Col>
       </Row>
       <Row>
